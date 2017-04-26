06c7b3cee68d214b82a3a8a7861ce94c53768de6
Douglas Bacon <dugbdev@gmail.com>
2017-04-25 20:12:44 -0400

    completed lazyloading of images

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./app/assets/scripts/app.js",
+  entry: {
+    App: "./app/assets/scripts/app.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: path.join(__dirname, "/app/temp/scripts"),
-    filename: "app.js"
+    filename: "[name].js"
   },
   module: {
     loaders: [
