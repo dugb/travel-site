014ec20343b78840846cfe41c63aaabe62ea3808
Douglas Bacon <dugbdev@gmail.com>
2017-06-06 22:20:07 -0400

    added brower support fallbacks

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,6 +5,11 @@ var gulp        = require('gulp'),
     convert     = require('gulp-rsvg');
 
 var config = {
+  shape: {
+    spacing: {
+      padding: 1
+    }
+  },
   mode: {
     css: {
       variables: {
